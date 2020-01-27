048353056d35c1f54e3d36e4c2816a03e7f8910a
Gurava <Akash_Chandra@Dell.com>
2020-01-27 20:39:55 +0530

    remove unnecessary files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+var app = express();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const TorrentSearchApi = require("torrent-search-api");
+const ptt = require("parse-torrent-title");
+
+const makeResponse = (status, reason, data) => {
+  return {
+    status,
+    reason,
+    data
+  };
+};
+
+TorrentSearchApi.enableProvider('1337x');
+TorrentSearchApi.enableProvider('Rarbg');
+
+app.get("/", async (request, response) => {
+  const query = request.query.query;
+  if (query) {
+    const category = request.query.category || "All";
+    const limit = parseInt(request.query.limit) || 20;
+
+    try {
+      const results = await TorrentSearchApi.search(query, category, 1);
+      const resultsDetailed = results.map(result => {
+        const title = result.title;
+        const data = ptt.parse(title);
+        data.link = result.magnet;
+        return Object.assign(result, data);
+      });
+      response.send(makeResponse("success", {}, resultsDetailed));
+    } catch (error) {
+      response.status(423);
+      response.send(makeResponse("error", error, []));
+    }
+  } else {
+    response.status(422);
+    response.send(makeResponse("error", { info: "query is mandatory" }, []));
+  }
 });
 
-module.exports = router;
+app.listen(3000)
